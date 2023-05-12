c063169e4213968c3c63dd6f8ce4790a92184fca
leothefrench <104638709+leothefrench@users.noreply.github.com>
2023-05-12 14:23:01 +0200

    Recherche implémenter - Essai à faire

--- js/templates/search.js
+++ js/templates/search.js
@@ -3,32 +3,44 @@
 /* ADD EVENT LISTENER SUR INPUT DE L'UTILISATEUR DANS LA BARRE PRINCIPALE DE RECHERCHE */
 document.querySelector('.inputSearch').addEventListener('keyup', (e) => {
     const dataSearch = e.target.value.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, ""); // INPUT USER IN PRINCIPAL SEARCH
+    console.log(dataSearch);
 
-    principalSearch(dataSearch, recipes) // INVOCATION FONCTION PRINCIPALSEARCH
+    principalSearch(dataSearch, recipes)
 })
 
-/* FUNCTION DE RECHERCHE BARRE PRINCIPALE COMMENCANT A PARTIR DE 3 CARACTERES */
+const tag = [] // POUR L'AJOUT DES VALEURS DES TAGS QUE L'ON VA RECUPERER ???
+
+/* FUNCTION DE RECHERCHE BARRE PRINCIPALE COMMENCANT AVEC AU MOINS DE 3 CARACTERES */
 function principalSearch(userInput, recettes) {
-    if(userInput.length > 2 ) {
-        searchUser(userInput, recettes)
-    }
+
+    const recipesRemainingAfterSearch = searchUser(userInput, recettes) 
+    console.log(recipesRemainingAfterSearch);
+    const recettesFinales =  searchByTags(recipesRemainingAfterSearch)
+
+    hydrateInterface(recettesFinales)
 }
 
-/* FUNCTION FILTER BY NAME - PRINCIPAL SEARCH BAR */
-function searchUser(userInput, arrayRecipes) {
-    const dataFilterByName = arrayRecipes.filter((item) => {
-        return (item.name.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(userInput))
-    })
-    console.log(dataFilterByName); // OK
 
-    /* RECIPE FILTERED BY NAME WITHOUT DUPLICATE */
+searchByTags(recipes) // RECHERCHE DIRECTE PAR LES CHAMPS AVANCES
+
+/* SEARCH BY PRINCIPAL BAR */
+function searchUser(userInput, arrayRecipes) {
+    if(userInput.length < 3) return arrayRecipes
+
+    /* FUNCTION FILTER BY NAME */
+    const dataFilterByName = arrayRecipes.filter((item) => {
+        return (item.name.toLowerCase().includes(userInput))
+    })
+    // console.log(dataFilterByName);
+
+    /* RECIPES REMAINING FILTERED BY NAME WITHOUT DUPLICATE */
     let recipeArrayFilterByName = [...new Set(dataFilterByName.map((item) => {return item}))]
-    // console.log(recipeArrayFilterByName);
+    console.log(recipeArrayFilterByName);
 
     /* RECIPES FILTERED BY INGREDIENTS */
     let allIngredientsOfRecipes = [];   // TEMPORARY ARRAY FOR CONTAINING AT THE END ALL INGREDIENTS
     arrayRecipes.map((arrayIngredients) => {
-        let tempArray = [] // ARRAY TEMPORAIRE QUI CONTIENDRA LES ARRAYS INGREDIENTS
+        let tempArray = [] // ARRAY TEMPORAIRE QUI CONTIENDRA LES ARRAYS INGREDIENTS DE CHQUE RECETTE
 
         arrayIngredients.ingredients.map(item => {
             tempArray.push(item.ingredient)
@@ -37,13 +49,12 @@ function searchUser(userInput, arrayRecipes) {
 
         const dataFilterByIngredients = tempArray.filter((item) => {
             if(item.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(userInput)) {  
-                allIngredientsOfRecipes.push(arrayIngredients) // ?? CELA NE MARCHE PAS POUR LIMONADE
+                allIngredientsOfRecipes.push(arrayIngredients)
             }
         }) 
     })
-    console.log(allIngredientsOfRecipes)    // CHECK ARRAY WITH ALL INGREDIENTS SEARCH BY THE USER IN THE PRINCIPAL SEARCH BAR
 
-    /* RECIPE FILTERED BY INGREDIENTS AND WITHOUT DUPLICATE */ 
+    /* RECIPES REMAINING FILTERED BY INGREDIENTS AND WITHOUT DUPLICATE */ 
     let dataFilterByIngredientsLast = [...new Set(allIngredientsOfRecipes.map((item) => {return item}))]
     console.log(dataFilterByIngredientsLast);
 
@@ -51,21 +62,21 @@ function searchUser(userInput, arrayRecipes) {
     const dataFilterByDescription = recipes.filter((item) => {
         return (item.description.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "").includes(userInput))
     })
-    console.log(dataFilterByDescription);
-    /* RECIPES FILTERED BY DESCRIPTION AND WIHOUT DUPLICATE */
+    // console.log(dataFilterByDescription);
+    /* RECIPES REMAINING FILTERED BY DESCRIPTION AND WIHOUT DUPLICATE */
     let recipeArrayFilterByDescription = [...new Set(dataFilterByDescription.map((item) => {return item}))]
     console.log(recipeArrayFilterByDescription)
 
     // ARRAY MERGE WITHOUT DUPLICATE IN ES6 - NEW SET WITH SPREAD OPERATOR
     const mergeArraysFilter = [...new Set([...recipeArrayFilterByName, ...dataFilterByIngredientsLast, ...recipeArrayFilterByDescription])]
-    console.log(mergeArraysFilter) // LOG DES RECETTES RESTANTES SANS DOUBLONS
-    
-    hydrateInterface(mergeArraysFilter) // INJECTION RECETTES RESTANTES DANS LE DOM APRES RECHERCHE UTILISATEUR (POINT 3)
+    console.log(mergeArraysFilter) 
 
-    champsIngredients(mergeArraysFilter) 
-    champsAppareils(mergeArraysFilter)
-    champsUstensiles(mergeArraysFilter)
 
+    ActualisationChampsIngredients(mergeArraysFilter) // FONCTION D'ACTUALISATION DU CHAMPS DE RECHERCHE AVANCE INGREDIENTS
+    ActualisationChampsAppareils(mergeArraysFilter)
+    actualisationChampsUstensiles(mergeArraysFilter)
+
+    return mergeArraysFilter; // RETURN ALL RECIPES REMAINING AFTER SEARCH BY NAME, INGREDIENTS & DESCRIPTION
 }
 
 /* FONCTION D'ACTUALISATION DE L'INTERFACE AVEC LES RECETTES RESTANTES APRES FILTRATION */
@@ -79,13 +90,83 @@ function hydrateInterface(arrRecipesRemaining) {
         classRecipesWrapper.innerHTML = ''   // CLEAN DOM RECIPES WRAPPER CONTAINER
         finalRecipeArray = Object.entries(arrRecipesRemaining);
 
-        finalRecipeArray.forEach(recipe => createCard(recipe));     // INJECTION IN THE DOM AVEC FONCTION CREATECARD (ModelRecipes.js)
+        finalRecipeArray.forEach(recipe => createCard(recipe)); // INJECTION DOM AVEC FONCTION CREATECARD (fichier ModelRecipes.js)
     }
 }
 
+/* IMPLEMENTATION DE LA FONCTION  GET TAG SEARCH */
+
+function searchByTags(recetteReduite) {
+    console.log(recetteReduite)
+    /* TAG INGREDIENTS ADDED */
+    const ingredientsElementsTags = document.querySelectorAll(".tagDesIngredients")
+    const ingredientsIterable = Array.from(ingredientsElementsTags)
+
+    let ingredients = ingredientsIterable.map((item) => {return item.innerText})
+    console.log(ingredients); // JE RECUPERE UN ARRAY AVEC LES INGREDIENTS RESTANTS DES RECETTES REDUITES
+         
+    /* TAG APPLIANCE ADDED */
+    const appliancesElements = document.querySelectorAll(".tagDesAppareils")
+    const appliancesIterable = Array.from(appliancesElements)
+ 
+    let appliance = appliancesIterable.map((item) => {return item.innerText})
+    console.log(appliance); // JE RECUPERE UN ARRAY AVEC LES APPAREILS RESTANTS DES RECETTES REDUITE
+
+   /* TAG USTENSILES ADDED */
+    const ustensilsElements = document.querySelectorAll(".tagDesUstensiles")
+    const ustensilsIterable = Array.from(ustensilsElements)
+
+    let ustensiles = ustensilsIterable.map((item) => {return item.innerText})
+    console.log(ustensiles); // JE RECUPERE UN ARRAY AVEC LES USTENSILES RESTANTS DES RECETTES REDUITE
+
+    const recettesFinales = recetteReduite.filter(recette => {
+
+        let hasIngredients = true;
+        let hasAppliance = true;
+        let hasUstensils = true;
+
+        let recette2 = recette.ingredients
+        // console.log(recette2);
+        let recette3 = recette2.map(item => {return item.ingredient})
+        // console.log(recette3);
+
+        ingredients.forEach(ingredient => {
+            if (!recette3.includes(ingredient)) { // CA BUG
+                hasIngredients = false;
+            }
+        })
+
+        appliance.forEach(appliance => {
+            if (!recette.appliance.includes(appliance)) {
+                hasAppliance = false;
+            }
+        })
+
+        ustensiles.forEach(ustensil => {
+            console.log(recette.ustensils)
+            if (!recette.ustensils.includes(ustensil)) {
+                hasUstensils = false;
+            }
+        })
+
+        if (hasIngredients && hasAppliance && hasUstensils) {
+            return true
+        } else {
+            return false
+        }
+    })
+
+    ActualisationChampsIngredients(recettesFinales) // FONCTION DE MISE A JOUR DES CHAMPS DE RECHERCHE AVANCEE
+    ActualisationChampsAppareils(recettesFinales)
+    actualisationChampsUstensiles(recettesFinales)
+    console.log(recettesFinales);
+    hydrateInterface(recettesFinales)
+    return recettesFinales;
+}   
+
 /* FONCTION DE MISE A JOUR DES CHAMPS DE RECHERCHE AVANCES - INGREDIENTS, APPAREILS, USTENSILES */
 
-function champsIngredients(recettes) { // ON PASSE LES RECIPES RESTANTES APRES FILTRATION - NAME - INGREDIENTS - DESCRIPTION
+function ActualisationChampsIngredients(recettes) { // ON PASSE LES RECIPES RESTANTES APRES FILTRATION - NAME - INGREDIENTS - DESCRIPTION
     let tagByIngredient = []; // VA RECEVOIR TOUS LES INGREDIENTS
     
     const ingredientsArrRecipe = recettes.map((arrIngredient) => {
@@ -111,7 +192,7 @@ function champsIngredients(recettes) { // ON PASSE LES RECIPES RESTANTES APRES F
         const listElementsTagIngredients = document.querySelector('.listElementsIngredients')
         listElementsTagIngredients.innerHTML = listItems // INJECTION DANS LE CHAMPS INGREDIENTS
 
-        /* L'UTILISATEUR PRECISE SA RECHERCHE AVEC LE CHAMPS INGREDIENTS (POINT 5) */
+        /* L'UTILISATEUR PRECISE SA RECHERCHE AVEC LE CHAMPS INPUT INGREDIENTS (POINT 5) */
         document.querySelector('.inputSearchBtnIngredients').addEventListener('keyup', (e) => {
             const inputSearchTag = e.target.value.toLowerCase(); 
 
@@ -131,12 +212,12 @@ function champsIngredients(recettes) { // ON PASSE LES RECIPES RESTANTES APRES F
             const listElementsTagIngredients = document.querySelector('.listElementsIngredients') // HYDRATE LA LISTE DES ELEMENTS
             listElementsTagIngredients.innerHTML = listItemsTagSecondary
 
-            choiceChamps('listElementsIngredients', 'resultTag:first-child', 'resultTag:nth-child(2)', 'resultTag:last-child' ) // FONCTION QUI ECOUTE l'EVENEMENT CLICK SUR INGREDIEN T      
+            addTagIngredients()     
         })
     })
 }
 /* CHAMPS APPAREILS */
-function champsAppareils(recettes) {
+function ActualisationChampsAppareils(recettes) {
     let tagApplianceAfterPrincipaleSearch = [] // ARRAY VA RECEVOIR LES APPAREILS
 
     recettes.forEach(item => {
@@ -167,21 +248,23 @@ function champsAppareils(recettes) {
                 afterFilterByTagAppliances.push(item)
             }
         })
-        let listItemsTagSecondary = afterFilterByTagAppliances.map(item => {
-            console.log(item);
+
+        const tagByAppliancesNoDuplicate = [...new Set(afterFilterByTagAppliances.map((item) => {return item}))]
+
+        let listItemsTagSecondary = tagByAppliancesNoDuplicate.map(item => {
             return '<p>' + item + '</p>'
         })
         
-        console.log(afterFilterByTagAppliances)
+        console.log(tagByAppliancesNoDuplicate)
 
         const listElementsTagAppliances = document.querySelector('.listElementsAppareils')
         listElementsTagAppliances.innerHTML = listItemsTagSecondary // HYDRATATION APRES RECHERCHE UTILISATEUR
 
-        choiceChamps('listElementsAppareils', 'resultTag:nth-child(2)', 'resultTag:last-child', 'resultTag:first-child')  // FONCTION QUI ECOUTE l'EVENEMENT CLICK SUR APPAREILS
+        addTagAppliances()
     })
 }
 /* CHAMPS USTENSILES */
-function champsUstensiles(recettes) {
+function actualisationChampsUstensiles(recettes) {
     let tagUtensilsAfterPrincipaleSearch = [] // ARRAY QUI VA RECEVOIR LES USTENSILES DES RECIPES
     const tagFilterByUstensilsSecondary =  recettes.map(ele => {
         ele.ustensils.filter((item) => {
@@ -215,68 +298,137 @@ function champsUstensiles(recettes) {
             }
         })
 
-        console.log(arrTagUstensilesSecondaryNoDuplicate);
-
         let listItemsTagSecondary = afterFilterByTagUstensiles.map(item => {
-            // console.log(item);
             return '<p>' + item + '</p>'
         })
+
         const listElementsTagUstensiles = document.querySelector('.listElementsUstensiles')
         listElementsTagUstensiles.innerHTML = listItemsTagSecondary
 
-        choiceChamps('listElementsUstensiles', 'resultTag:last-child', 'resultTag:first-child', 'resultTag:nth-child(2)')
+        addTagUstensiles()
     })
 }
 
-/* L'UTILISATEUR CHOISIT UN MOT DANS UN DES CHAMPS QUI AFFICHE LE TAG & DISPLAY NONE LES 2 AUTRES TAGS */
+// FONCTION DE RECHERCHE DIRECT PAR TAG QUI ACTUALISE DES ELEMENTS DE CHAQUE CHAMPS DE RECHERCHE AVANCEE
+ActualisationChampsIngredients(recipes) 
+ActualisationChampsAppareils(recipes)
+actualisationChampsUstensiles(recipes)
 
-function choiceChamps(nameListChamps, resultChampsChild, brotherOneTag, brotherTwoTag) {
-    const listOfChamps = document.querySelectorAll(`.${nameListChamps} > p`)
+/* DEFINITION DE LA FONCTION QUI RECOIT LE CLIQUE UTILISATEUR SUR LE CHAMPS INGREDIENTS ET AJOUTE LE TAG OU LES TAGS */
+
+function addTagIngredients() {
+    const listOfChamps = document.querySelectorAll('.listElementsIngredients > p')
     const listing = [...listOfChamps] // TRANSFORME EN ARRAY AVEC SPREAD OPERATOR
 
-    listing.forEach((item) => {
-        item.addEventListener('click', function() {
+    listing.forEach(item => {
+        item.addEventListener('click', function(e) {
+            e.preventDefault()
+            const groupTags = document.querySelector('.groupResultsTags')
+            groupTags.style.display = 'flex'
+            const buttonTag = document.createElement('button')
+            buttonTag.classList.add('resultTag')
+            buttonTag.style.backgroundColor = '#3282F7';
+            const spanButton = document.createElement('span')
+            spanButton.classList.add('tagDesIngredients')
+            spanButton.innerText = `${item.innerText}`
+            const crossInsideSpan = document.createElement('i')
+            crossInsideSpan.classList.add('fa-regular')
+            crossInsideSpan.classList.add('fa-circle-xmark')
+            spanButton.appendChild(crossInsideSpan)
+            buttonTag.appendChild(spanButton)
 
-        const groupTags = document.querySelector('.groupResultsTags')
+            // buttonTag.innerHTML = `<span class='tagDesIngredients'>${item.innerText}<i class="fa-regular fa-circle-xmark"></i></span>`
+            groupTags.appendChild(buttonTag)
+            console.log('POUF ONE')
 
-        const groupTagsIngredients = document.querySelector(`.${resultChampsChild}`)
-        groupTagsIngredients.innerHTML = item.innerText
-        groupTags.style.display = 'flex'
-  
-        const tagBrotherOneDisplay = document.querySelector(`.${brotherOneTag}`)
-        tagBrotherOneDisplay.style.display = 'none'
+            let cross = document.querySelectorAll('.fa-circle-xmark')
+ 
+            cross.forEach(item => {
+                item.addEventListener('click', function() {
+                    document.querySelector('.resultTag').style.display = 'none';
+                    hydrateInterface(recipes)
 
-        const tagBrotherTwoDisplay = document.querySelector(`.${brotherTwoTag}`)
-        tagBrotherTwoDisplay.style.display = 'none'
+                    ActualisationChampsIngredients(recipes) 
+                    ActualisationChampsAppareils(recipes)
+                    actualisationChampsUstensiles(recipes)
 
-        let value = item.innerText
-        console.log(value) // J'AI LA VALUE  ET MAINTENANT IL FAUT LA récuperer puis HYDRATER LA PAGE WEB MAIS COMMENT ?
+                })
+            })
+            searchByTags(recipes) // ICI UN BUG ???
+       })
+    })  
+}
 
+addTagIngredients()
 
+/* DEFINITION DE LA FONCTION QUI RECOIT LE CLIQUE UTILISATEUR SUR LE CHAMPS APPAREILS ET AJOUTE LE TAG OU LES TAGS */
+function addTagAppliances() {
+    const listOfChamps = document.querySelectorAll('.listElementsAppareils > p')
+    const listing = [...listOfChamps] // TRANSFORME EN ARRAY AVEC SPREAD OPERATOR
+
+    listing.forEach(item => {
+        item.addEventListener('click', function(e) {
+            e.preventDefault()
+            const groupTags = document.querySelector('.groupResultsTags')
+            groupTags.style.display = 'flex'
+            const buttonTag = document.createElement('button')
+            buttonTag.classList.add('resultTag')
+            buttonTag.style.backgroundColor = '#68D9A4';
+            buttonTag.innerHTML = `<span class='tagDesAppareils'>${item.innerText}<i class="fa-regular fa-circle-xmark"></i></span>`
+            groupTags.appendChild(buttonTag)
+            console.log('POUF 2')
+
+            let cross = document.querySelectorAll('.fa-circle-xmark')
+
+            cross.forEach(item => {
+                item.addEventListener('click', function() {
+                    document.querySelector('.resultTag').style.display = 'none';
+                    hydrateInterface(recipes)
+
+                    ActualisationChampsIngredients(recipes) 
+                    ActualisationChampsAppareils(recipes)
+                    actualisationChampsUstensiles(recipes)
+                })
+            })
+            searchByTags(recipes)
         })
     })
 }
 
+addTagAppliances()
 
-/* A2 */
-champsIngredients(recipes)
-champsAppareils(recipes)
-champsUstensiles(recipes)
+/* DEFINITION DE LA FONCTION QUI RECOIT LE CLIQUE UTILISATEUR SUR LE CHAMPS USTENSILES ET AJOUTE LE TAG OU LES TAGS */
+function addTagUstensiles() {
+    const listOfChamps = document.querySelectorAll('.listElementsUstensiles > p')
+    const listing = [...listOfChamps] // TRANSFORME EN ARRAY AVEC SPREAD OPERATOR
 
-// INTERSECTION DES ARRAYS SERACH BY TAGS
+    listing.forEach(item => {
+        item.addEventListener('click', function(e) {
+            e.preventDefault()
+            const groupTags = document.querySelector('.groupResultsTags')
+            groupTags.style.display = 'flex'
+            const buttonTag = document.createElement('button')
+            buttonTag.classList.add('resultTag')
+            buttonTag.style.backgroundColor = '#ED6454';
+            buttonTag.innerHTML = `<span class='tagDesUstensiles'>${item.innerText}<i class="fa-regular fa-circle-xmark"></i></span>`
+            groupTags.appendChild(buttonTag)
+            console.log('POUF 3') 
 
+            let cross = document.querySelectorAll('.fa-circle-xmark')
 
+            cross.forEach(item => {
+                item.addEventListener('click', function() {
+                    document.querySelector('.resultTag').style.display = 'none';
+                    hydrateInterface(recipes)
 
-/* A3 */
-/* POUR CHAQUE BOUTON, RECUPERER L'EVENEMENT CLICK, DECLENCHER UNE FONCTION QUI AJOUTE UN ELEMENT TAG
-CORRESPONDANT A SA CATEGORIE*/
-function addTag() {
-    const listOfChamps = document.querySelectorAll(`.${nameListChamps} > p`)
-    const listing = [...listOfChamps]
-
-    listing.forEach((item) => {
-        item.addEventListener('click', function() {
-        
+                    ActualisationChampsIngredients(recipes) 
+                    ActualisationChampsAppareils(recipes)
+                    actualisationChampsUstensiles(recipes)
+                })
+            })
+            searchByTags(recipes)
         })
     })
 }
+
+addTagUstensiles()
